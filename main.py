2099ac00ab29d691e2360d1ff5db7a665c528a3f
Max Kendall <66796793+maxsrobotics@users.noreply.github.com>
2024-08-18 21:01:49 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# Un-named-for-now-
+print ("You wake up in a dark vally. yet you don't really know how you got here. Nothing is connecting. It feels... eerie.")
+print ("??? : 'Where... Where am I? What is this place? ")
+print("Wait. Who am I? Maybe I am tresspasing? I got to look around.")
+print (
+  "Looking around, you see darkness. nothing but darkness. You can't see. ??? : I gotta get out of here. I have a bad feeling. ")
+print (
+  "??? : Ouch! What was tha...?")
+print(
+  "??? : If you don't mind me asking, why are you glowing?")
+print ("Unknown : I'm glowing because I drank a potion to make me glow. It is the only way to see in the dark mate. Why are you here without the effect of the potion?")
+print (
+  "What do you tell him?")
+print (
+  "1. I don't know. I just woke up here. I don't know how I got here. I need some info.")
+print ("2. I, um, kinda forgot.")
+print ("3. That, is none of your business,'mate'") 
+print ("Please input the Number 1, 2 3, and whatever number pops up on your screen that you want to choose.")
+user_input = input()
+if user_input == "1" : print ("Unknown : I think I know what happened to you.")
+if user_input == "2" : print ("Unknown : That is dumb. NOBODY would forget that. Your a pretty bad lier.")
+if user_input == "3" : print ("Unknown : Yeah, I think I know what happend.")
+print("??? = How do you know?")
+print ("Random = 'Listen. I know how to get you out of here. If you want to get out of here and your precious little memories? Follow me' ")
+print("Will you follow him?")
+print ("1.Yes")
+print ("2. No")
+user_input = input()
+if user_input = "1" : print ("You follow him to this strange hallway. It was... unsettling. Like the screaming of thousands of people. ??? = You sure this is the right place? Random = Yeah, I think so.")
+if user_input = "2" : print ("You decide to not follow him. You felt like you were in danger with him around. Like HE was the danger.                       Random : Ok kid, your choice. If you do decied you need me for something, ask someone to lead you to MR. CLEAN. There, you will find me.")
